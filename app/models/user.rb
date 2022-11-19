702d66bb1cd4822232f7aa52676ff7d43c55e06f
rb01penguin <rb01penguin@gmail.com>
2022-11-19 23:48:48 +0900

    deviseのviewファイル（編集用）を生成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, length: { maximum: 20 } # 追加
+  validates :nickname, presence: true # 追加
+end
